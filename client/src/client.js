b586538732e9788be87aed788e0a15301d6301e7
David Du <davdu21@bergen.org>
2020-11-22 13:50:56 -0500

    Fixed issue of being able to placing a chip on the other players and replaces it

---
+++ client/src/client.js
@@ -0,0 +1,103 @@
+//https://www.youtube.com/watch?v=P2i11xnrpNI
+
+const writeEvent = (text) => {
+    // <ul> element
+    const parent = document.querySelector('#events');
+
+    // <li> element
+    const el = document.createElement('li');
+    el.innerHTML = text;
+
+    parent.appendChild(el);
+};
+
+const onFormSubmitted = (e) => {
+    e.preventDefault();
+
+    const input = document.querySelector('#chat');
+    const text = input.value;
+    input.value = '';
+
+    sock.emit('message', text);
+};
+
+/*const placeEgg = (text) => {
+    document.getElementById('color').textContent = "red";
+    const box = document.querySelector(text);
+    box.classList.add('red');
+};*/
+
+writeEvent('Welcome to RPS');
+
+const sock = io();
+//sock.on('message', writeEvent);
+//sock.on('red', placeEgg);
+sock.on('message', function(index) {
+    document.getElementById('color').textContent = index;
+    if (index !== "Hi, you are connected"){
+        console.log(index);
+
+        const box = document.getElementById(index);
+        box.classList.add('red');
+    }
+
+});
+
+
+document
+    .querySelector('#chat-form')
+    .addEventListener('submit', onFormSubmitted);
+
+
+// ------------------------ mouse position -----------
+window.addEventListener('mousemove', function (e){
+    document.getElementById('x-value').textContent = e.x;
+    document.getElementById('y-value').textContent = e.y;
+
+});
+
+
+// https://www.youtube.com/watch?v=W0No1JDc6vE
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    let width = 15;
+    let squares = [];
+    let count = 0;
+
+    //create Board
+    function createBoard() {
+        for (let i = 0; i < width*width; i++){
+            const square = document.createElement('div');
+            square.setAttribute('id', i);
+            grid.appendChild(square);
+            squares.push(square);
+
+            square.addEventListener('click', function(e) {
+                click(square)
+            })
+        }
+    }
+
+    createBoard();
+
+    function click(square) {
+        if (count % 2 === 0){
+            //square.classList.add('red');
+            const ind = squares.indexOf(square);
+            const index = ind;
+            sock.emit('message', index);
+        }
+        else{
+            if(!( square.classList.contains('red'))){
+                square.classList.add('blue');
+            }
+            else{
+                count--;
+            }
+
+            //sock.emit('message', 'blue');
+        }
+        count++;
+    }
+
+});
---
+++ server/server.js
@@ -0,0 +1,36 @@
+// https://www.youtube.com/watch?v=xVcVbCLmKew
+
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+
+const clientPath = `${__dirname}/../client`;
+//const clientPath = `/Users/wonjunlee/WebstormProjects/game_lab/client/gomoku.html`;
+console.log(`Serving static from ${clientPath}`);
+
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+io.on('connection', (sock) => {
+    console.log('Someone connected');
+    sock.emit('message', 'Hi, you are connected');
+
+    sock.on('message', (text) => {
+        io.emit('message', text);
+    });
+});
+
+server.on('error', (err) => {
+    console.error('Server error:', err);
+});
+
+server.listen(8080, () => {
+    console.log('RPS started on 8080');
+});
+
+
